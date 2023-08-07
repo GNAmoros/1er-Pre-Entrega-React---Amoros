5a23e84577716859e208efa50d74b10446f782b9
Nicolas Amoros <nicolas.amoros@gmail.com>
2023-08-06 22:56:47 -0300

    woking on css

--- src/componentes/Navbar/Navbarcomp.jsx
+++ src/componentes/Navbar/Navbarcomp.jsx
@@ -1,14 +1,15 @@
 import React, { Component } from 'react';
 import { Navbar, Container, Nav, Form, Button } from 'react-bootstrap';
 import CartWidget from '../cartWidget/cartWidget';
+import './navbar.css';
 
 export default class Navbarcomp extends Component {
     render() {
         return (
             <div>
-                <Navbar expand="lg" className="bg-body-tertiary">
+                <Navbar expand="lg" className="navbarStyle">
                     <Container fluid>
-                        <Navbar.Brand href="#">PadelPro</Navbar.Brand>
+                        <Navbar.Brand href="#" className="navTitle">PadelPro</Navbar.Brand>
                         <Navbar.Toggle aria-controls="navbarScroll" />
                         <Navbar.Collapse id="navbarScroll">
                             <Nav
@@ -16,8 +17,8 @@ export default class Navbarcomp extends Component {
                                 style={{ maxHeight: '100px' }}
                                 navbarScroll
                             >
-                                <Nav.Link href="#action1">Palas</Nav.Link>
-                                <Nav.Link href="#action2">Indumentaria</Nav.Link>
+                                <Nav.Link href="#action1" className="navType">Palas</Nav.Link>
+                                <Nav.Link href="#action2" className="navType">Indumentaria</Nav.Link>
                             </Nav>
                             <Form className="d-flex">
                                 <Form.Control
@@ -26,7 +27,7 @@ export default class Navbarcomp extends Component {
                                     className="me-2"
                                     aria-label="Search"
                                 />
-                                <Button variant="outline-success">Buscar</Button>
+                                <Button variant="outline-success" className="navSearch">Buscar</Button>
                             </Form>
                             {/* CartWidget */}
                             <CartWidget addToCart={this.addToCart} removeFromCart={this.removeFromCart} />
